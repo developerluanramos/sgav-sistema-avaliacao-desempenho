1a40173ef984c8e4bb447c5a282a411caeb3b572
Your Name <you@example.com>
2023-08-18 16:21:17 -0300

    SGPE-2 - cadastro de fornecedores

--- resources/views/app/fornecedor/partials/form.blade.php
+++ resources/views/app/fornecedor/partials/form.blade.php
@@ -1,12 +1,8 @@
-@if($errors->any())
-    @foreach($errors as $error)
-        {{$error}}
-    @endforeach
-@endif
-
 @csrf
-<input type="text" name="razao_social" value="{{$forncedor->razao_social ?? @old('razao_social') }}">
+
+<input type="text" name="razao_social" value="{{$forncedor->razao_social ?? old('razao_social') }}">
 <br>
-<input type="text" name="nome_fantasia" value="{{$forncedor->nome_fantasia ?? @old('nome_fantasia') }}">
+<input type="text" name="nome_fantasia" value="{{$forncedor->nome_fantasia ?? old('nome_fantasia') }}">
 <br>
 <input type="submit" value="save">
+
