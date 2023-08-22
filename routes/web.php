96bae73d21c1c46cbf11d78579dac0a6ce82e236
Your Name <you@example.com>
2023-08-22 14:09:44 -0300

    teste

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,36 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get("/test-01", function () {
+   function playGame(array $gameFormation): int {
+       $score = [];
+
+       foreach ($gameFormation as $index => $gameCaracter ) {
+           echo ("<pre>".json_encode($score)."</pre>");
+           switch ($gameCaracter) {
+               case '+':
+                   $score[] = $score[$index-1] + $score[$index-2];
+                   break;
+
+               case 'C':
+                   unset($score[count($score)-1]);
+                   break;
+
+               case 'D':
+                   //$score[] = $score[$index-1] * 2;
+                   break;
+
+               default:
+                   $score[] = $gameCaracter;
+                   break;
+           }
+       }
+        //dd($score);
+       return array_sum($score);
+   }
+
+   echo playGame(explode(" ", "5 2 C D +"));
+});
 
 Route::get('fornecedor', [\App\Http\Controllers\App\Fornecedor\FornecedorIndexController::class, 'index'])->name('fornecedor.index');
 Route::get('fornecedor/create', [\App\Http\Controllers\App\Fornecedor\FornecedorCreateController::class, 'create'])->name('fornecedor.create');
