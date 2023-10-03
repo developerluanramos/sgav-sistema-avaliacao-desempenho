84b0001d63487fed46ab4d1dc4928b0adccec622
kaikiyuuji <yuuji.hirata@kolaresti.com.br>
2023-10-03 11:45:59 -0300

    SGPE-15 Index controller e request

--- app/Http/Controllers/App/Servidor/ServidorIndexController.php
+++ app/Http/Controllers/App/Servidor/ServidorIndexController.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Controllers\App\Servidor;
 
+use App\Actions\Servidor\ServidorIndexAction;
 use App\Http\Controllers\Controller;
-
+use App\Models\Servidor;
+use Illuminate\Http\Request;
 
 class ServidorIndexController extends Controller
 {
+    public function __construct(
+        protected ServidorIndexAction $indexAction
+    ) {}
 
-    public function index()
+    public function index(Request $request)
     {
-        return view('app.servidor.index');
+        $servidores = Servidor::with('cargo')
+            ->paginate($request->get('totalPerPage', 15));
+
+        return view('app.servidor.index', compact('servidores'));
     }
 }
