9c64b07023c0c3b4e478788d35a6c804091fe2ea
Luan Ramos <luanramos@Luans-MacBook-Air.local>
2023-10-16 10:51:46 -0300

    corrige link menu

--- resources/views/app/layouts/app.blade.php
+++ resources/views/app/layouts/app.blade.php
@@ -100,9 +100,17 @@ data-te-navbar-ref>
       <li class="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
         <a
           class="text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-          href="#"
+          href="{{route('servidor.index')}}"
           data-te-nav-link-ref
           >Servidores</a
+        >
+      </li>
+      <li class="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+        <a
+          class="text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+          href="#"
+          data-te-nav-link-ref
+          >Cargos</a
         >
       </li>
     </ul>
