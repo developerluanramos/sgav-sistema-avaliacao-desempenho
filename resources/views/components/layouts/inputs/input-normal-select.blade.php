da1baa673cb2db53ca4c552af3171fa28cf29cd9
= <=>
2023-10-19 12:03:29 -0300

    SGAD-37 - Corrige componente input-select

--- resources/views/components/layouts/inputs/input-normal-select.blade.php
+++ resources/views/components/layouts/inputs/input-normal-select.blade.php
@@ -6,7 +6,17 @@
         <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-black py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="{{$name}}" name="{{$name}}">
             <option value=""></option>
             @foreach($data as $cargo)
-                <option value="{{ $cargo->uuid }}">{{ $cargo->nome }}</option>
+                @if (isset($value) && !is_null($value))
+                 @php
+                 $selected= '';
+                     if ($cargo->uuid == $value) {
+                        $selected = "selected";
+                     }
+                 @endphp
+                    <option {{$selected}} value="{{ $cargo->uuid }}">{{ $cargo->nome }}</option>
+                @else
+                    <option value="{{ $cargo->uuid }}">{{ $cargo->nome }}</option>
+                @endif
             @endforeach
         </select>
         <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
