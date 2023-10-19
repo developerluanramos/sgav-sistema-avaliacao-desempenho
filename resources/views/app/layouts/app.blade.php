b238a315964292d81260ccdbe6cc648a5b7678de
kaikiyuuji <yuuji.hirata@kolaresti.com.br>
2023-10-19 16:24:37 -0300

    SGAD-35 - Menu Usuarios

--- resources/views/app/layouts/app.blade.php
+++ resources/views/app/layouts/app.blade.php
@@ -113,6 +113,14 @@ data-te-navbar-ref>
           >Cargos</a
         >
       </li>
+      <li class="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+        <a
+          class="text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+          href="{{route('usuario.index')}}"
+          data-te-nav-link-ref
+          >Usuários</a
+        >
+      </li>
     </ul>
   </div>
 
