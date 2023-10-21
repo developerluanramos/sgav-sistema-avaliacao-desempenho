388ab6466ddec55204d2334bcc309802d608bf23
= <=>
2023-10-20 23:38:28 -0300

    SGAD-39 - dark mode

--- resources/views/components/layouts/inputs/input-date.blade.php
+++ resources/views/components/layouts/inputs/input-date.blade.php
@@ -1,5 +1,5 @@
 <div class="w-full md:w-{{$lenght}} px-3 mb-6 md:mb-0">
-    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="{{ $name }}">
+    <label class="block uppercase tracking-wide text-xs font-bold mb-2" for="{{ $name }}">
         {{ $label }}
     </label>
     <input
