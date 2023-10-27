fb07dafb7f658efa9729d230172fcf3f408a141c
kaikiyuuji <yuuji.hirata@kolaresti.com.br>
2023-10-27 10:19:51 -0300

    SGAD-75 - Rotas web

--- routes/web.php
+++ routes/web.php
@@ -2,33 +2,22 @@
 
 use App\Http\Controllers\App\Dashboard\DashboardIndexController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\App\Servidor\ServidorCreateController;
-use App\Http\Controllers\App\Servidor\ServidorEditController;
-use App\Http\Controllers\App\Servidor\ServidorIndexController;
-use App\Http\Controllers\App\Servidor\ServidorShowController;
-use App\Http\Controllers\App\Servidor\ServidorStoreController;
-use App\Http\Controllers\App\Servidor\ServidorUpdateController;
-use App\Http\Controllers\App\Usuario\UsuarioCreateController;
-use App\Http\Controllers\App\Usuario\UsuarioEditController;
-use App\Http\Controllers\App\Usuario\UsuarioIndexController;
-use App\Http\Controllers\App\Usuario\UsuarioShowController;
-use App\Http\Controllers\App\Usuario\UsuarioStoreController;
-use App\Http\Controllers\App\Usuario\UsuarioUpdateController;
-use App\Models\Servidor;
 
-Route::get('usuario/show/{uuid}', [UsuarioShowController::class, 'show'])->name('usuario.show');
-Route::get('/usuario/create', [UsuarioCreateController::class, 'create'])->name('usuario.create');
-Route::put('/usuario/{user}', [UsuarioUpdateController::class, 'update'])->name('usuario.update');
-Route::get('/usuario', [UsuarioIndexController::class, 'index'])->name('usuario.index');
-Route::get('/usuario/edit/{user}', [UsuarioEditController::class, 'edit'])->name('usuario.edit');
-Route::post('/usuario', [UsuarioStoreController::class, 'store'])->name('usuario.store');
+Route::get('/equipe/create', [\App\Http\Controllers\App\Equipe\EquipeCreateController::class, 'create'])->name('equipe.create');
 
-Route::get('servidor/show/{uuid}', [ServidorShowController::class, 'show'])->name('servidor.show');
-Route::get('/servidor/create', [ServidorCreateController::class, 'create'])->name('servidor.create');
-Route::put('/servidor/{servidor}', [ServidorUpdateController::class, 'update'])->name('servidor.update');
-Route::get('/servidor', [ServidorIndexController::class, 'index'])->name('servidor.index');
-Route::get('/servidor/edit/{servidor}', [ServidorEditController::class, 'edit'])->name('servidor.edit');
-Route::post('/servidor', [ServidorStoreController::class, 'store'])->name('servidor.store');
+Route::get('usuario/show/{uuid}', [\App\Http\Controllers\App\Usuario\UsuarioShowController::class, 'show'])->name('usuario.show');
+Route::get('/usuario/create', [\App\Http\Controllers\App\Usuario\UsuarioCreateController::class, 'create'])->name('usuario.create');
+Route::put('/usuario/{user}', [\App\Http\Controllers\App\Usuario\UsuarioUpdateController::class, 'update'])->name('usuario.update');
+Route::get('/usuario', [\App\Http\Controllers\App\Usuario\UsuarioIndexController::class, 'index'])->name('usuario.index');
+Route::get('/usuario/edit/{user}', [\App\Http\Controllers\App\Usuario\UsuarioEditController::class, 'edit'])->name('usuario.edit');
+Route::post('/usuario', [\App\Http\Controllers\App\Usuario\UsuarioStoreController::class, 'store'])->name('usuario.store');
+
+Route::get('servidor/show/{uuid}', [\App\Http\Controllers\App\Servidor\ServidorShowController::class, 'show'])->name('servidor.show');
+Route::get('/servidor/create', [\App\Http\Controllers\App\Servidor\ServidorCreateController::class, 'create'])->name('servidor.create');
+Route::put('/servidor/{servidor}', [\App\Http\Controllers\App\Servidor\ServidorUpdateController::class, 'update'])->name('servidor.update');
+Route::get('/servidor', [\App\Http\Controllers\App\Servidor\ServidorIndexController::class, 'index'])->name('servidor.index');
+Route::get('/servidor/edit/{servidor}', [\App\Http\Controllers\App\Servidor\ServidorEditController::class, 'edit'])->name('servidor.edit');
+Route::post('/servidor', [\App\Http\Controllers\App\Servidor\ServidorStoreController::class, 'store'])->name('servidor.store');
 
 Route::get('/', [DashboardIndexController::class, 'index'])->name('dashboard.index');
 
@@ -38,7 +27,6 @@ Route::post('fornecedor', [\App\Http\Controllers\App\Fornecedor\FornecedorStoreC
 Route::get('fornecedor/{uuid}/show', [\App\Http\Controllers\App\Fornecedor\FornecedorShowController::class, 'show'])->name('fornecedor.show');
 Route::get('fornecedor/{uuid}/edit', [\App\Http\Controllers\App\Fornecedor\FornecedorEditController::class, 'edit'])->name('fornecedor.edit');
 Route::put('fornecedor/{uuid}/update', [\App\Http\Controllers\App\Fornecedor\FornecedorUpdateController::class, 'update'])->name('fornecedor.update');
-
 
 Route::get('cargo/create', [\App\Http\Controllers\App\Cargo\CargoCreateController::class, 'create'])->name('cargo.create');
 Route::post('cargo', [\App\Http\Controllers\App\Cargo\CargoStoreController::class, 'store'])->name('cargo.store');
