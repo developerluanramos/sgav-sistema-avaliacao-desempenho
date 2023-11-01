e850d08f293e3117ee316116fbf3aea90d3ef108
kaikiyuuji <yuuji.hirata@kolaresti.com.br>
2023-11-01 11:48:02 -0300

    SGAD-76 Removendo Return Action Create

--- app/Actions/Equipe/EquipeCreateAction.php
+++ app/Actions/Equipe/EquipeCreateAction.php
@@ -7,15 +7,12 @@ use App\Repositories\Equipe\EquipeRepositoryInterface;
 class EquipeCreateAction
 {
     public function __construct(
-        protected EquipeRepositoryInterface $equipeRepository
     ) { }
 
     public function exec(): array
     {
-        $equipes = $this->equipeRepository->all();
-        
         return [
-            "equipes" => $equipes,
+
         ];
     }
 }
