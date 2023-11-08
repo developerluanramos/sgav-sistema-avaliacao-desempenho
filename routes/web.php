8c21fa686668e2bdfbd8c62bb4d2a9481ab29a0e
kaikiyuuji <yuuji.hirata@kolaresti.com.br>
2023-11-08 18:00:22 -0300

    SGAD-82 Rotas de Conceito Avaliacao

--- routes/web.php
+++ routes/web.php
@@ -54,4 +54,10 @@ Route::middleware(['auth.basic'])->group(function() {
     Route::get('modelo-avaliacao/{uuid}/edit', [\App\Http\Controllers\App\ModeloAvaliacao\ModeloAvaliacaoEditController::class, 'edit'])->name('modelo-avaliacao.edit');
     Route::put('modelo-avaliacao/{uuid}/update', [\App\Http\Controllers\App\ModeloAvaliacao\ModeloAvaliacaoUpdateController::class, 'update'])->name('modelo-avaliacao.update');
 
+    Route::get('conceito-avaliacao/create', [\App\Http\Controllers\App\ConceitoAvaliacao\ConceitoAvaliacaoCreateController::class, 'create'])->name('conceito-avaliacao.create');
+    Route::post('conceito-avaliacao', [\App\Http\Controllers\App\ConceitoAvaliacao\ConceitoAvaliacaoStoreController::class, 'store'])->name('conceito-avaliacao.store');
+    Route::get('conceito-avaliacao', [\App\Http\Controllers\App\ConceitoAvaliacao\ConceitoAvaliacaoIndexController::class, 'index'])->name('conceito-avaliacao.index');
+    Route::get('conceito-avaliacao/{uuid}/show', [\App\Http\Controllers\App\ConceitoAvaliacao\ConceitoAvaliacaoShowController::class, 'show'])->name('conceito-avaliacao.show');
+    Route::get('conceito-avaliacao/{uuid}/edit', [\App\Http\Controllers\App\ConceitoAvaliacao\ConceitoAvaliacaoEditController::class, 'edit'])->name('conceito-avaliacao.edit');
+    Route::put('conceito-avaliacao/{uuid}/update', [\App\Http\Controllers\App\ConceitoAvaliacao\ConceitoAvaliacaoUpdateController::class, 'update'])->name('conceito-avaliacao.update');
 });
