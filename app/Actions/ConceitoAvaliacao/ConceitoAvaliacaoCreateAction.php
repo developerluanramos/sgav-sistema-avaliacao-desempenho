63697df689f81faced9a7d23aadccda416bc728d
kaikiyuuji <yuuji.hirata@kolaresti.com.br>
2023-11-08 18:03:12 -0300

    SGAD-82 - Logica do Metodo Create de Conceitos

---
+++ app/Actions/ConceitoAvaliacao/ConceitoAvaliacaoCreateAction.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Actions\ConceitoAvaliacao;
+
+class ConceitoAvaliacaoCreateAction
+{
+    public function __construct(
+    ) { }
+
+    public function exec(): array
+    {
+        return [];
+    }
+}
