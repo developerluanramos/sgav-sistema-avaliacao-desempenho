0268bb6cb6e21f98b52e354a30e3f15ef5029c4d
kaikiyuuji <yuuji.hirata@kolaresti.com.br>
2023-11-09 17:24:41 -0300

    SGAP-82 - Logica de criação e remoção de itens na view

--- resources/views/app/conceito-avaliacao/create.blade.php
+++ resources/views/app/conceito-avaliacao/create.blade.php
@@ -15,3 +15,4 @@
 
 @endsection
 
+
--- resources/views/app/conceito-avaliacao/partials/form.blade.php
+++ resources/views/app/conceito-avaliacao/partials/form.blade.php
@@ -15,28 +15,5 @@
     />
 </div>
 
-<div class="itens">
-    <div class="flex flex-wrap -mx-3 mt-2">
-        <x-layouts.inputs.input-array-text
-            label="Item"
-            name="itens_conceitos_avaliacao[0][nome]"
-            lenght="8/12"
-            :value="$conceitoAvaliacao->nome ?? old('nome')"
-        />
-        <x-layouts.inputs.input-normal-number
-            label="Pontuacao"
-            name="itens_conceitos_avaliacao[0][pontuacao]"
-            lenght="2/12"
-            :value="$conceitoAvaliacao->pontuacao ?? old('pontuacao')"
-        />
-        <x-layouts.buttons.simple-button
-            text="Excluir"
-            onclick=""
-            color="danger"
-            lenght="2/12"
-        />
-    </div>
-</div>
-
 <x-layouts.inputs.input-item/>
 <x-layouts.buttons.submit-button text="Salvar"/>
