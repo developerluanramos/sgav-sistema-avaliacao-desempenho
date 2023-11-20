199873723031b10411759c11f6db35a7701e2571
Thyerre Sampaio <thyerre322@gmail.com>
2023-11-20 15:13:52 -0300

    SGAD-83 - alteracao componente action button

--- resources/views/app/modelo-avaliacao/fator-avaliacao/partials/list.blade.php
+++ resources/views/app/modelo-avaliacao/fator-avaliacao/partials/list.blade.php
@@ -1,32 +1,33 @@
-<ul class="w-full text-sm flex flex-col items-start font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-    @foreach($fatoresAvaliacao as $index => $fatorAvaliacao)
-        <li class="w-full flex px-4 py-2 uppercase font-bold border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-            {{ $fatorAvaliacao->nome }}
+@if($fatoresAvaliacao->count())
+    <ul class="w-full text-sm flex flex-col mb-4 items-start font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+        @foreach($fatoresAvaliacao as $index => $fatorAvaliacao)
+            <li class="w-full flex px-4 py-2 uppercase font-bold border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+                {{ $fatorAvaliacao->nome }}
 
-            <form method="POST" action="{{route('fator-avaliacao.update', $fatorAvaliacao->uuid)}}" class="flex ml-auto items-center">
-                @csrf
-                @method('PUT')
-                <x-layouts.modals.simple-modal :titulo="'Edição'">
-                    @section('modal-content')
-                            @include('app.modelo-avaliacao.fator-avaliacao.partials.form', ["fatorAvaliacao" => $fatorAvaliacao])
-                    @endsection
-                </x-layouts.modals.simple-modal>
-                <x-layouts.buttons.edit-action-button
-                        color="primary"
-                />
-            </form>
+                <form method="POST" action="{{route('fator-avaliacao.update', $fatorAvaliacao->uuid)}}" class="flex ml-auto items-center">
+                    @csrf
+                    @method('PUT')
+                    <x-layouts.modals.simple-modal :titulo="'Edição'">
+                        @section('modal-content')
+                                @include('app.modelo-avaliacao.fator-avaliacao.partials.form', ["fatorAvaliacao" => $fatorAvaliacao])
+                        @endsection
+                    </x-layouts.modals.simple-modal>
+                    <x-layouts.buttons.edit-action-button
+                            color="primary"
+                    />
+                </form>
 
-            <form method="POST" action="/fator-avaliacao/{{ $fatorAvaliacao->uuid }}" class="flex items-center">
-                @csrf
-                @method('DELETE')
-                <div class="m-1"></div>
-                <x-layouts.buttons.submit-delete-button
-                    color="danger"/>
-            </form>
-        </li>
-    @endforeach
-</ul>
-
+                <form method="POST" action="/fator-avaliacao/{{ $fatorAvaliacao->uuid }}" class="flex items-center">
+                    @csrf
+                    @method('DELETE')
+                    <div class="m-1"></div>
+                    <x-layouts.buttons.submit-delete-button
+                        color="danger"/>
+                </form>
+            </li>
+        @endforeach
+    </ul>
+@endif
 
 
 
--- resources/views/components/layouts/buttons/action-button.blade.php
+++ resources/views/components/layouts/buttons/action-button.blade.php
@@ -40,29 +40,27 @@
     </a>
 
 @elseif($action == "excluir")
-    <a href="{{$route}}">
         <button
             type="button"
             class="rounded bg-{{$color}} px-1 pb-1 pt-1 text-sm font-medium text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
             data-te-ripple-init
             data-te-ripple-color="light"
-            data-drawer-target="drawer-example" 
-            data-drawer-show="drawer-example"  
-            aria-controls="drawer-example">
+            data-drawer-target="drawer-delete-confirmation" 
+            data-drawer-show="drawer-delete-confirmation"  
+            aria-controls="drawer-delete-confirmation">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 22 22" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
             <path stroke-linecap="round" stroke-linejoin="round" d="M17.293 6.293a1 1 0 00-1.414 0L11 10.586l-4.293-4.293a1 1 0 10-1.414 1.414L9.586 12l-4.293 4.293a1 1 0 101.414 1.414L11 13.414l4.293 4.293a1 1 0 001.414-1.414L12.414 12l4.293-4.293a1 1 0 000-1.414z"/>
         </svg>
         </button>
-    </a>
 @else
     {{$text}}
 @endif
 
-<div id="drawer-example" class="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-white w-80 dark:bg-gray-800" tabindex="-1" aria-labelledby="drawer-label">
+<div id="drawer-delete-confirmation" class="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-white w-80 dark:bg-gray-800" tabindex="-1" aria-labelledby="drawer-label">
     <h5 id="drawer-label" class="inline-flex items-center mb-4 text-base font-semibold text-gray-500 dark:text-gray-400"><svg class="w-4 h-4 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
      <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
    </svg>Info</h5>
-    <button type="button" data-drawer-hide="drawer-example" aria-controls="drawer-example" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white" >
+    <button type="button" data-drawer-hide="drawer-delete-confirmation" aria-controls="drawer-delete-confirmation" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white" >
        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
        </svg>
@@ -72,7 +70,7 @@
     <p class="mb-6 text-sm text-gray-500 dark:text-gray-400">Supercharge your hiring by taking advantage of our <a href="#" class="text-blue-600 underline dark:text-blue-500 hover:no-underline">limited-time sale</a> for Flowbite Docs + Job Board. Unlimited access to over 190K top-ranked candidates and the #1 design job board.</p>
     <div class="grid grid-cols-2 gap-4">
        <a href="#" class="px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-200 rounded-lg focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Learn more</a>
-       <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Get access <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+       <a href="{{$route}}" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Get access <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
    </svg></a>
     </div>
