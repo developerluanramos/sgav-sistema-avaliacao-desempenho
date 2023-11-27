492d0691f440659880ed513d216c88582dcd43d4
kaikiyuuji <yuuji.hirata@kolaresti.com.br>
2023-11-27 10:57:51 -0300

    SGAD-50 Lista de Indicadores

--- resources/views/app/modelo-avaliacao/indicadores-desempenho/partials/list.blade.php
+++ resources/views/app/modelo-avaliacao/indicadores-desempenho/partials/list.blade.php
@@ -1,11 +1,7 @@
-@php
-    $faker = Faker\Factory::create();
-@endphp
-
 <ul class="max-w-md ml-4 py-2 space-y-1 text-gray-700 list-disc list-inside dark:text-gray-400">
-    @for($i = 0; $i < 5; $i++)
+    @foreach($indicadoresDesempenho as $indicadorDesempenho)
         <li>
-            {{ $faker->catchPhrase }}
+            {{ $indicadorDesempenho->descricao }}
         </li>
-    @endfor
+    @endforeach
 </ul>
