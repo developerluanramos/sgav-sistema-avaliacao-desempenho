2c9e2af3111fb2923bb1e92f1ce83b00b439a828
Luan Ramos <luan.ramos@kolaresti.com.br>
2023-11-29 21:38:39 -0300

    corrige listagem de indicadores desempenho

--- resources/views/app/modelo-avaliacao/indicadores-desempenho/partials/list.blade.php
+++ resources/views/app/modelo-avaliacao/indicadores-desempenho/partials/list.blade.php
@@ -1,7 +1,9 @@
-<ul class="max-w-md ml-4 py-2 space-y-1 text-gray-700 list-disc list-inside dark:text-gray-400">
-    @foreach($indicadoresDesempenho as $indicadorDesempenho)
-        <li>
-            {{ $indicadorDesempenho->descricao }}
-        </li>
-    @endforeach
-</ul>
+@if($indicadoresDesempenho->count())
+    <ul class="max-w-md ml-4 py-2 space-y-1 text-gray-700 list-disc list-inside dark:text-gray-400">
+        @foreach($indicadoresDesempenho as $indicadorDesempenho)
+            <li>
+                {{ $indicadorDesempenho->descricao }}
+            </li>
+        @endforeach
+    </ul>
+@endif
