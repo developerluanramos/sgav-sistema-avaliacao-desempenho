a3989fac3be1196e58c4f8c30fa94b81917e42df
Thyerre Sampaio <thyerre322@gmail.com>
2023-12-05 18:03:12 -0300

    SGAD-119 - DTO setor update

--- app/DTO/Setor/SetorStoreDTO.php
+++ app/DTO/Setor/SetorStoreDTO.php
@@ -2,7 +2,20 @@
 
 namespace App\DTO\Setor;
 
+use App\Http\Requests\App\Setor\SetorStoreRequest;
+
 class SetorStoreDTO
 {
+    public function __construct(
+        public string $nome,
+        public string $postos_trabalho_uuid
+    ) {}
 
+    public static function makeFromRequest(SetorStoreRequest $request): self 
+    {
+        return new self(
+            $request->nome,
+            $request->postos_trabalho_uuid
+        );
+    }
 }
