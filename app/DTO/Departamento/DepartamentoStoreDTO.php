3907b364ba79bc1d4d23faeb5aa40803dbfb347b
kaiki <yuuji.hirata@kolaresti.com.br>
2023-12-08 11:13:10 -0300

    SGAD-124 Arquivos para logica de create departamentos

--- app/DTO/Departamento/DepartamentoStoreDTO.php
+++ app/DTO/Departamento/DepartamentoStoreDTO.php
@@ -4,5 +4,18 @@ namespace App\DTO\Departamento;
 
 class DepartamentoStoreDTO
 {
+    public function __construct(
+        public string $nome,
+        public string $postos_trabalho_uuid,
+        public string $setor_uuid
+    ) {}
 
+    public static function makeFromRequest(SetorStoreRequest $request): self
+    {
+        return new self(
+            $request->nome,
+            $request->postos_trabalho_uuid,
+            $request->setor_uuid,
+        );
+    }
 }
